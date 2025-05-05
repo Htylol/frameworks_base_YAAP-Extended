5a46d148aa62e70f87c46ebd62c9b0c66ed40949
Guidix <guidixm@gmail.com>
2025-05-05 21:09:39 +0300

    base: Fixup hideapplist package name

--- core/java/com/android/internal/util/axion/HideAppListUtils.java
+++ core/java/com/android/internal/util/axion/HideAppListUtils.java
@@ -1,4 +1,4 @@
-package com.android.internal.util.axion;
+package com.android.internal.util.yaap;
 
 import android.content.ContentResolver;
 import android.content.Context;
--- services/core/java/com/android/server/pm/ComputerEngine.java
+++ services/core/java/com/android/server/pm/ComputerEngine.java
@@ -182,7 +182,7 @@ import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
 
-import com.android.internal.util.axion.HideAppListUtils;
+import com.android.internal.util.yaap.HideAppListUtils;
 
 /**
  * This class contains the implementation of the Computer functions.  It
